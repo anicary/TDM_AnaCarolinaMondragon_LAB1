62e6167de8954c38c142227d56f376db2becb8d8
name <carobox96@gmail.com>
2018-02-10 22:05:11 -0700

    Initial commit

---
+++ app/src/main/java/mx/edu/ittepic/tdm_anacarolinamondragon_lab1/MainActivity.java
@@ -0,0 +1,47 @@
+package mx.edu.ittepic.tdm_anacarolinamondragon_lab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText valor;
+    Button mult;
+    TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        valor = findViewById(R.id.valor);
+        mult = findViewById(R.id.mult);
+        result = findViewById(R.id.result);
+
+        mult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String salida ="";
+                int resultado;
+                valor.setText("");
+
+                String valor1 = valor.getText().toString();
+                int tabla = Integer.parseInt(valor1);
+
+                for(int i=0; i<=20;i++){
+                    resultado = tabla*i;
+                    salida += tabla+"x"+i+"="+resultado;
+
+                    result.setText("\n"+salida);
+
+                }
+
+
+
+            }
+        });
+    }
+}
